ee680f9f78d2cb3b43ddc31942cd7e29139b8375
butlerc9 <butlerc9@tcd.ie>
2023-05-27 16:12:48 +0100

    working training network

--- python_code/main.py
+++ python_code/main.py
@@ -16,15 +16,24 @@ from network_classes.fully_connected import fully_connected_layer
 from network_classes.activation_layer import activation_layer
 from helper_functions.activation_funcs import sigmoid, der_sigmoid
 
-df = pd.read_csv('data/raw/mnist_train.csv',nrows=2)
+df = pd.read_csv('data/raw/mnist_train.csv', nrows=1000)
 y_train = df['label'].to_numpy()
 x_train = df.drop('label',axis=1).to_numpy()
 
-net = network()
-net.add_layer(fully_connected_layer(784, 6))
-net.add_layer(fully_connected_layer(6, 5))
-net.add_layer(fully_connected_layer(5, 4))
-net.add_layer(fully_connected_layer(4, 10))
+n_values = np.max(9) + 1
+y_train = np.eye(n_values)[y_train]
 
+# transpose operations require a 2d array 
+# e.g. a normal row vector (12,) transposed still gives (12,) whereas a (12,1 vector) transposed gives (1,12)
+y_train = np.expand_dims(y_train,axis=1)
+x_train = np.expand_dims(x_train,axis=2)
+
+net = network()
+net.add_layer(fully_connected_layer(784, 10))
+net.add_layer(activation_layer(sigmoid, der_sigmoid))
+net.add_layer(fully_connected_layer(10, 10))
+net.add_layer(activation_layer(sigmoid, der_sigmoid))
+
+# train network
 net.train(x_train, y_train, 0.1)
 
--- python_code/network_classes/network.py
+++ python_code/network_classes/network.py
@@ -6,15 +6,15 @@ class network():
     def __init__(self):
         self.layers = []
         self.total_loss = 0.0
+        self.correct_predictions_during_current_epoch = 0
 
     def add_layer(self, layer: layer):
         self.layers.append(layer)
 
     def calculate_cost_vector(self, x: int, y: int) -> np.array:
-        self.total_loss += ((x - y) ** 2).sum() # This is the average for one training example, we will take the average of this over the epoch
-        cost_vector = 2 * (x - y)
-        print(cost_vector.shape)
-        # return output gradient        
+        self.total_loss += ((x - y.T) ** 2).sum() # This is the average for one training example, we will take the average of this over the epoch
+        cost_vector = 2 * (x - y.T)
+        # return output gradient      
         return cost_vector
     
     def feed_forward_single_example(self, x: int):
@@ -39,13 +39,14 @@ class network():
 
     
     def train(self, x_train: np.array, y_train: np.array, learning_rate: float) -> np.array:
-
-        for i in range(10):
+        
+        for i in range(100): # run 10 epochs
             for x, y in zip(x_train,y_train):
                 final_layer_output = self.feed_forward_single_example(x)
                 network_output_derivative = self.calculate_cost_vector(final_layer_output, y)
                 self.back_propagation_single_example(network_output_derivative, learning_rate)
             
+            print(self.total_loss/y_train.shape[0])
             self.total_loss = 0
 
             
